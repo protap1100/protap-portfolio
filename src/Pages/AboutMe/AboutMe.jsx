4a617257cf9d6dc9ea85b261668df3371bbd4ec9
protap1100 <protapb23@gmail.com>
2024-07-02 23:40:17 +0600

    add:contact use form

--- src/Pages/AboutMe/AboutMe.jsx
+++ src/Pages/AboutMe/AboutMe.jsx
@@ -3,9 +3,10 @@ import image from "../../assets/me.jpg";
 // import image2 from "../../assets/about.png";
 import { useTypewriter } from "react-simple-typewriter";
 import facebook from '../../assets/Social-Icons/facebook.png'
-import instagram from '../../assets/Social-Icons/instagram.png'
+// import instagram from '../../assets/Social-Icons/instagram.png'
 import twitter from '../../assets/Social-Icons/twitter.png'
 import github from '../../assets/Social-Icons/github.png'
+import linkedin from '../../assets/Social-Icons/linkedin.png'
 const AboutMe = () => {
   const [text] = useTypewriter({
     words: [" Programmer", " Front End Developer", " Coder"],
@@ -23,13 +24,13 @@ const AboutMe = () => {
        <h1 className=" text-2xl lg:text-4xl  text-center font-bold">I Am A <span className="text-purple-700">{text}</span> </h1>
        <h1 className="text-center">Currently Learning Mern Stack. Building Project on MERN Stack. </h1>
        <div className="flex gap-5">
-            <Link to='#'> <img className="h-12 w-12" src={facebook} alt="" /> </Link>
-            <Link to='#'> <img className="h-12 w-12" src={instagram} alt="" /> </Link>
-            <Link to='#'> <img className="h-12 w-12" src={github} alt="" /> </Link>
-            <Link to='#'> <img className="h-12 w-12" src={twitter} alt="" /> </Link>
+            <Link to='https://www.facebook.com/protap.biswas1100'> <img className="h-12 w-12" src={facebook} alt="" /> </Link>
+            <Link to='https://www.linkedin.com/in/protap-biswas1100/'> <img className="h-12 w-12" src={linkedin} alt="" /> </Link>
+            <Link to='https://github.com/protap1100'> <img className="h-12 w-12" src={github} alt="" /> </Link>
+            <Link to='https://twitter.com/protapb110'> <img className="h-12 w-12" src={twitter} alt="" /> </Link>
        </div>
        <div className="flex gap-2 flex-col lg:flex-row">
-        <Link className="bg-btn px-5 py-3 rounded font-bold hover:bg-btn-hover text-white">Download Cv</Link>
+        <Link to='https://drive.google.com/file/d/1cV3KBG5B8V_tkgVqfRkX_TncC-Pcinx-/view?usp=drive_link' className="bg-btn px-5 py-3 rounded font-bold hover:bg-btn-hover text-white">Download Cv</Link>
         <Link className="bg-btn px-5 py-3 rounded font-bold hover:bg-btn-hover text-white">Contact Me</Link>
        </div>
       </div>
